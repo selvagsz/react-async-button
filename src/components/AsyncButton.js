81366d0ea0333a23ecc165d7bab8d1f7c9578302
selvagsz <selvaganeshbsg@gmail.com>
2016-06-20 18:31:28 +0530

    Add dependent packages

--- src/components/AsyncButton.js
+++ src/components/AsyncButton.js
@@ -1,61 +1,61 @@
 export default React.createClass({
-	getInitialState() {
-		return {
-			pending: false,
-			promiseFulFilled: false,
-			promiseRejected: false
-		};
-	},
+  getInitialState() {
+    return {
+      pending: false,
+      promiseFulFilled: false,
+      promiseRejected: false
+    };
+  },
 
-	resetState() {
-		this.setState({
-			pending: false,
-			promiseFulfilled: false,
-			promiseRejected: false
-		});
-	},
+  resetState() {
+    this.setState({
+      pending: false,
+      promiseFulfilled: false,
+      promiseRejected: false
+    });
+  },
 
-	handleClick() {
-		this.setState({
-			pending: true
-		});
+  handleClick() {
+    this.setState({
+      pending: true
+    });
 
-		let promise = this.props.onClick(...arguments);
-		if (promise && promise.then) {
-			promise.then(() => {
-				this.setState({
-					pending: false,
-					promiseRejected: false,
-					promiseFulfilled: true
-				});
-			}).catch((error) => {
-				this.setState({
-					pending: false,
-					promiseRejected: true,
-					promiseFulfilled: false
-				});
-				throw error;
-			});
-		} else {
-			this.resetState();
-		}
-	},
+    let promise = this.props.onClick(...arguments);
+    if (promise && promise.then) {
+      promise.then(() => {
+        this.setState({
+          pending: false,
+          promiseRejected: false,
+          promiseFulfilled: true
+        });
+      }).catch((error) => {
+        this.setState({
+          pending: false,
+          promiseRejected: true,
+          promiseFulfilled: false
+        });
+        throw error;
+      });
+    } else {
+      this.resetState();
+    }
+  },
 
-	render() {
-		let isPending = this.state.pending;
-		let isFulfilled = this.state.promiseFulfilled;
-		let isRejected = this.state.promiseRejected;
+  render() {
+    let isPending = this.state.pending;
+    let isFulfilled = this.state.promiseFulfilled;
+    let isRejected = this.state.promiseRejected;
     let isDisabled = this.props.disabled || isPending;
-		let buttonText;
+    let buttonText;
 
-		if (isPending) {
-			buttonText = this.props.pendingText;
-		} else if (isFulfilled) {
-			buttonText = this.props.fulFilledText;
-		} else if (isRejected) {
-			buttonText = this.props.rejectedText;
-		}
-		buttonText = buttonText || this.props.text;
+    if (isPending) {
+      buttonText = this.props.pendingText;
+    } else if (isFulfilled) {
+      buttonText = this.props.fulFilledText;
+    } else if (isRejected) {
+      buttonText = this.props.rejectedText;
+    }
+    buttonText = buttonText || this.props.text;
 
 		return (
       <button {...this.props} disabled={isDisabled} onClick={this.handleClick}>
